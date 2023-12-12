2300eddbce8018fed8de5e0af812d2a7ca447ac3
veix <veixq9@protonmail.com>
2023-12-12 14:04:05 +0100

    add notes.clj

---
+++ notes.clj
@@ -0,0 +1,8 @@
+(ns )
+
+
+
+"usage of manifold.stream lib for composition of components into a single IO unit, reducing UΙ clutter and cognitive load"
+"shell for quick fuzzy search of components for quick insertion"
+"webization"
+
